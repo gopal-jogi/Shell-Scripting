ec39493a025867ecadf2966515cba98d9fa78da4
gopal-jogi <gopaljogi5@gmail.com>
2024-01-27 21:21:47 +0530

    update

--- ShellScripting/practice/addcontent.sh
+++ ShellScripting/practice/addcontent.sh
@@ -1,1 +1,15 @@
-#! /bin
+#! /bin/bash
+echo "enter the file name"
+read file
+if [ -e "$file" ]; then
+	echo "$file exit"
+	echo "Enter the sentence to add to the file:"
+	read new
+	echo "$new" >> "$file"
+else
+	echo "$file does not exit"
+fi
+
+while read line; do
+	echo "$line"
+done < "$file"
--- ShellScripting/practice/script11.sh
+++ ShellScripting/practice/script11.sh
@@ -1,19 +1,2 @@
-#! /bin/bash
-a="admin"
-b="secret"
-
-echo "Enter Username"
-read u
-echo "Enter Password"
-read p
-
-until [ $a == "$u" ] && [ $b == "$p" ];
-do
-	echo "invalide input"
-	echo "Enter Username"
-	read u
-	echo "Enter Password"
-	read p
-done
-echo "login"
-
+gopal jogi
+hi my name is gopal
